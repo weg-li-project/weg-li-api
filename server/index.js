b24329a8db5fd2275b47ab056259864758a1ae13
Jacob Rödel <roedel@campus.tu-berlin.de>
2020-11-26 22:42:11 +0100

    Add redirect from http to https routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const deleteUser = require("./controllers/user-deletion")
 const getSignedStorageUrls = require('./controllers/image-upload')
 
 const router = express.Router();
+
+if (process.env.NODE_ENV === "production") {
+    router.use((req, res, next) => {
+        req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+    })
+}
 router.use("/users/:user_id", deleteUser)
 router.use("/users", createUser)
 router.get('/analyze/image/upload', getSignedStorageUrls)
