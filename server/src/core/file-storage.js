054c4f873096e5244b4a4b3a8faa0358227e26b2
Jacob Rödel <roedel@campus.tu-berlin.de>
2020-11-30 22:14:05 +0100

    Add documentation to file storage class

--- server/src/core/file-storage.js
+++ server/src/core/file-storage.js
@@ -1,17 +1,24 @@
 const { Storage } = require('@google-cloud/storage')
 const uuid = require('uuid')
 
-const storage = new Storage()
-const bucket = storage.bucket(BUCKET_NAME)
 const EXPIRATION_TIME = 15 * 60 * 1000 // 15 minutes
 const BUCKET_NAME = 'weg-li_images'
 const DELIMITER = '/'
+
+const storage = new Storage()
+const bucket = storage.bucket(BUCKET_NAME)
 
 /**
  * Responsible for calling Google Cloud Storage APIs.
  */
 class FileStorage {
-    static async listFilesByPrefix(prefix, delimiter, bucketName) {
+    /**
+     * Gets a list of all files stored under a specified prefix.
+     *
+     * @param {String} prefix - A prefix, typically a folder name, which needs to be matched.
+     * @returns {Promise<*[string]>} Name of all found files.
+     */
+    static async listFilesByPrefix(prefix) {
         const options = {
             prefix: prefix[prefix.length - 1] === '/' ? prefix : `${prefix}/`,
             delimiter: DELIMITER
@@ -22,15 +29,31 @@ class FileStorage {
         return files.map(file => file.name)
     }
 
-    static async generateV4UploadSignedUrls(bucketName, folderName, amount) {
+    /**
+     * Provides a given amount of signed urls stored in specific folder
+     * on Google Cloud Storage.
+     *
+     * @param folderName - Name of folder.
+     * @param amount - Number of expected signed urls to create.
+     * @returns {Promise<[String]>} Array of signed urls
+     * @see {@link generateV4UploadSignedUrl}
+     */
+    static async generateV4UploadSignedUrls(folderName, amount) {
         const urls = []
         for (let i = 0; i < amount; ++i) {
-            const signedUploadUrl = await this.generateV4UploadSignedUrl(bucketName, `${folderName}/${i.toString()}.jpg`)
+            const signedUploadUrl = await this.generateV4UploadSignedUrl(`${folderName}/${i.toString()}.jpg`)
             urls.push(signedUploadUrl)
         }
         return urls
     }
 
+    /**
+     * Signs a given filename to enable arbitrary users to upload
+     * files to a predefined bucket location for a limited time.
+     *
+     * @param {String} filename - Name of the file.
+     * @returns {Promise<string>} Signed Url
+     */
     static async generateV4UploadSignedUrl(filename) {
         const options = {
             version: 'v4',
@@ -67,6 +90,11 @@ class FileStorage {
         }
     }
 
+    /**
+     * Creates a unique folder name that doesn't exists yet.
+     *
+     * @returns {Promise<string>} A unique folder name
+     */
     static async getUniqueFolderName() {
         let folderName = uuid.v4()
         let files = await FileStorage.listFilesByPrefix(folderName)
