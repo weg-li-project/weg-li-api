e67fa9ff6c18f5e86049587e6277bb1a4f678a55
niclasku <niclas-k@t-online.de>
2021-01-05 20:13:05 +0100

    Add user history based recommendations

--- server/src/core/recommender.js
+++ server/src/core/recommender.js
@@ -1,86 +1,168 @@
-'use strict'
-
-const ReportDatabaseHandle = require("./database/database-reports");
+const ReportDatabaseHandle = require('./database/database-reports');
 
 const NEAR_RADIUS = 50;
 const WIDE_RADIUS = 2000;
 const REPORTS_AROUND = 200;
 
+const USER_MULTIPLIER = 0.36;
+const LOCATION_MULTIPLIER = 1.64;
+const COMMON_MULTIPLIER = 0.01;
+
+/** TODO - faster sum - faster getMostCommon() */
+
 /**
- * Responsible for creating location based and user
- * data based recommendations.
+ * Responsible for creating recommendations based on user history and location.
  *
  * @author Niclas Kühnapfel
  */
 class Recommender {
-    dbHandle = new ReportDatabaseHandle();
+  constructor() {
+    this.dbHandle = new ReportDatabaseHandle();
+  }
 
-    async getRecommendations(location) {
-        //let mostCommon = await this.dbHandle.getMostCommonViolations();
-        let rec = new Recommendation(this.dbHandle);
-        //await rec.assignMostCommonScores(mostCommon);
-        await rec.assignLocationScores(location);
-        return rec.getSortedKeys();
-    }
-}
+  /**
+   * Returns ordered list of violation types. Most probable type on top.
+   *
+   * @param location The location of the new report.
+   * @param userId The user identifier for the new report.
+   * @returns {Promise<number[]>} Ordered list array of violation types.
+   */
+  async getRecommendations(location, userId = null) {
+    let allScores = {};
 
-class Recommendation {
-    violations = {};
+    const commonScores = await this.computeMostCommonScores();
+    allScores = this.constructor.addScores(
+      allScores,
+      commonScores,
+      COMMON_MULTIPLIER
+    );
 
-    constructor(dbHandle) {
-        this.dbHandle = dbHandle;
-    }
+    const locationScores = await this.computeLocationScores(location);
+    allScores = this.constructor.addScores(
+      allScores,
+      locationScores,
+      LOCATION_MULTIPLIER
+    );
 
-    async assignMostCommonScores(mostCommon) {
-        //let mostCommon = await this.dbHandle.getMostCommonViolations();
-        let len = Object.keys(mostCommon).length;
-        let violations = {};
-        mostCommon.forEach(type => {
-            violations[type] = 1 - (1 / len * mostCommon.indexOf(type));
-        });
-        this.assignScores(violations, 0)
-    }
-
-    static inverseQuadratic(reports) {
-        reports.forEach(function(report, index) {
-            let e = 0.1;
-            this[index].weight = 1 / (1 + (e * e * report.distance * report.distance));
-        }, reports);
-        return reports;
-    }
-
-    async assignLocationScores(location) {
-        let c = await this.dbHandle.countNearReports(location, NEAR_RADIUS);
-        let nearReports = await this.dbHandle.getKNN(location, c[0].count + REPORTS_AROUND, WIDE_RADIUS);
-        let reports = this.constructor.inverseQuadratic(nearReports);
-
-        // sum up weights to determine most probable violation type
-        let violations = {};
-        reports.forEach(function(report) {
-            if (report.violation_type in violations) {
-                violations[report.violation_type] += report.weight;
-            } else {
-                violations[report.violation_type] = report.weight;
-            }
-        });
-        this.assignScores(violations, 1);
+    if (userId !== null) {
+      const userScores = await this.computeUserHistoryScores(userId);
+      allScores = this.constructor.addScores(
+        allScores,
+        userScores,
+        USER_MULTIPLIER
+      );
     }
 
-    assignScores(violations, multiplier) {
-        Object.keys(violations).forEach(key => {
-            this.violations[key] = violations[key] + multiplier;
-        })
-    }
+    return this.constructor.getSortedKeys(allScores);
+  }
 
-    getSortedKeys() {
-        let keys = Object.keys(this.violations);
-        keys.sort((a, b) => {
-            return this.violations[b] - this.violations[a];
-        })
-        return keys.map(function (x) {
-            return parseFloat(x);
-        });
-    }
+  /**
+   * Computes scores based on most common violation types.
+   *
+   * @returns {Promise<{}>} List of violation types and their score
+   */
+  async computeMostCommonScores() {
+    const mostCommon = await this.dbHandle.getMostCommonViolations();
+    const len = Object.keys(mostCommon).length;
+
+    const scores = {};
+    mostCommon.forEach((type) => {
+      scores[type] = (len - mostCommon.indexOf(type)) / ((len * (len + 1)) / 2);
+    });
+
+    return scores;
+  }
+
+  /**
+   * Computes scores based on most common violation types in user's report history.
+   *
+   * @param userId The user's identifier.
+   * @returns {Promise<{}>} List of violation types and their score.
+   */
+  async computeUserHistoryScores(userId) {
+    const userHistory = await this.dbHandle.getUserReports(userId);
+
+    let count = 0;
+    userHistory.forEach((report) => {
+      count += parseInt(report.count, 10);
+    });
+
+    const scores = {};
+    userHistory.forEach((report) => {
+      scores[report.violation_type] = report.count / count;
+    });
+
+    return scores;
+  }
+
+  /**
+   * Computes scores based on location of the new report.
+   *
+   * @param location The location of the new report.
+   * @returns {Promise<{}>} List of violation types and their score.
+   */
+  async computeLocationScores(location) {
+    const count = await this.dbHandle.countNearReports(location, NEAR_RADIUS);
+    const reports = await this.dbHandle.getKNN(
+      location,
+      count + REPORTS_AROUND,
+      WIDE_RADIUS
+    );
+
+    // compute weights
+    let sum = 0;
+    reports.forEach(function (report, index) {
+      const e = 0.1;
+      const weight = 1 / (1 + e * e * report.distance * report.distance);
+      this[index].weight = weight;
+      sum += weight;
+    }, reports);
+
+    // sum up weights to determine most probable violation type
+    const scores = {};
+    reports.forEach((report) => {
+      if (report.violation_type in scores) {
+        scores[report.violation_type] += report.weight / sum;
+      } else {
+        scores[report.violation_type] = report.weight / sum;
+      }
+    });
+
+    return scores;
+  }
+
+  /**
+   * Combines scores and multiplies each score with specified multiplier.
+   *
+   * @param allScores Previous score list.
+   * @param scores New score list.
+   * @param multiplier Multiplier for new score list.
+   * @returns {any} Combined score list.
+   */
+  static addScores(allScores, scores, multiplier) {
+    const out = allScores;
+    Object.entries(scores).forEach((entry) => {
+      const [type, score] = entry;
+      if (out[type]) {
+        out[type] += score * multiplier;
+      } else {
+        out[type] = score * multiplier;
+      }
+    });
+    return out;
+  }
+
+  /**
+   * Sorts violation types based on their score.
+   *
+   * @param allScores Score list to sort.
+   * @returns {number[]} Sorted list of violation types.
+   */
+  static getSortedKeys(allScores) {
+    const keys = Object.keys(allScores);
+    keys.sort((a, b) => allScores[b] - allScores[a]);
+    return keys.map((x) => parseInt(x, 10));
+  }
 }
 
 module.exports = Recommender;
--- server/test/unit/core/recommender.test.js
+++ server/test/unit/core/recommender.test.js
@@ -1,65 +1,89 @@
-const Bluebird = require("bluebird");
+const Bluebird = require('bluebird');
 
-const Recommender = require("../../../src/core/recommender");
-const ReportDatabaseHandle = require("../../../src/core/database/database-reports");
-const Location = require("../../../src/models/location");
-const {Database, DatabaseConfiguration} = require("../../../src/core/database/database");
+const Recommender = require('../../../src/core/recommender');
+const ReportDatabaseHandle = require('../../../src/core/database/database-reports');
+const Location = require('../../../src/models/location');
+const {
+  Database,
+  DatabaseConfiguration,
+} = require('../../../src/core/database/database');
 
 function initDatabase() {
-    let configuration = DatabaseConfiguration.forTCPSocket("postgres", "postgres", "wegli_user_random", "localhost", "5432");
-    let database = new Database(configuration);
-    database.connect();
-    Database.shared = database;
+  const configuration = DatabaseConfiguration.forTCPSocket(
+    'postgres',
+    'postgres',
+    'wegli_user_random',
+    'localhost',
+    '5432'
+  );
+  const database = new Database(configuration);
+  database.connect();
+  Database.shared = database;
 }
 
 async function analyzeData(reports) {
-    let recommender = new Recommender();
-    let matches = [0, 0, 0, 0, 0]
-    let counter = 0;
+  const recommender = new Recommender();
+  const matches = new Array(44).fill(0);
+  let counter = 0;
 
-    // check for each datapoint
-    await Bluebird.map(reports, async (report) => {
-        let location = new Location(report.st_y, report.st_x);
-        let type = report.violation_type
+  // check for each data point
+  await Bluebird.map(
+    reports,
+    async (report) => {
+      const location = new Location(report.st_y, report.st_x);
+      const type = report.violation_type;
 
-        let recommendations = await recommender.getRecommendations(location);
+      const recommendations = await recommender.getRecommendations(
+        location,
+        report.user_id
+      );
 
-        if (recommendations.includes(type)) {
-            matches[recommendations.indexOf(type)] += 1;
-        }
-        counter += 1;
+      if (recommendations.includes(type)) {
+        matches[recommendations.indexOf(type)] += 1;
+      }
 
-    }, {concurrency: 1500});
+      counter += 1;
+    },
+    { concurrency: 2500 }
+  );
 
-    return [counter, matches];
+  return [counter, matches];
 }
 
 function printResults(counter, matches) {
-    matches.forEach(function (item, index) {
-        this[index] = 100 / counter * item;
-    }, matches);
-    let sum = matches[0] + matches[1] + matches[2];
+  matches.forEach(function (item, index) {
+    this[index] = (100 / counter) * item;
+  }, matches);
 
-    console.log("Analyzed " + counter.toString() + " data points.")
-    for (let i = 0; i < 5; i++) {
-        console.log((i + 1).toString() + ". recommendation accuracy: " + matches[i].toFixed(2).toString() + " %")
-    }
-    console.log("3 recommendations accuracy: " + (sum).toFixed(2).toString() + " %")
-    console.log("4 recommendations accuracy: " + (sum + matches[3]).toFixed(2).toString() + " %")
-    console.log("5 recommendations accuracy: " + (sum + matches[3] + matches[4]).toFixed(2).toString() + " %")
+  console.log(`Analyzed ${counter} data points.`);
+  for (let i = 0; i < 10; i += 1) {
+    console.log(
+      `${(i + 1).toString()}. recommendation accuracy: ${matches[i].toFixed(
+        2
+      )} %`
+    );
+  }
+
+  const sum = matches[0] + matches[1] + matches[2];
+  const sumFive = sum + matches[3] + matches[4];
+  const sumTen = sumFive + matches[5] + matches[6] + matches[7] + matches[8] + matches[9];
+
+  console.log(`3 recommendations accuracy: ${sum.toFixed(2)} %`);
+  console.log(`5 recommendations accuracy: ${sumFive.toFixed(2)} %`);
+  console.log(`10 recommendations accuracy: ${sumTen.toFixed(2)} %`);
 }
 
-describe(`Recommender`, function () {
-    describe("#getLocationRecommendations", function () {
-        it("shows overall accuracy", async function () {
-            initDatabase();
-            let dbHandle = new ReportDatabaseHandle();
-            let testReports = await dbHandle.getTestReports();
+describe('Recommender', () => {
+  describe('#getLocationRecommendations', () => {
+    it('shows overall accuracy', async () => {
+      initDatabase();
+      const dbHandle = new ReportDatabaseHandle();
+      const testReports = await dbHandle.getTestReports();
 
-            let analyzed = await analyzeData(testReports);
+      const analyzed = await analyzeData(testReports);
 
-            console.log("Overall Accuracy:")
-            printResults(analyzed[0], analyzed[1]);
-        }).timeout(720000);
-    })
-})
+      console.log('Overall Accuracy:');
+      printResults(analyzed[0], analyzed[1]);
+    }).timeout(0);
+  });
+});
