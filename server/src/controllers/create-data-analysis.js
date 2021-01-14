e2b3b10f458412712cda20515d86f7374539380a
niclasku <niclas-k@t-online.de>
2021-01-14 18:17:11 +0100

    Add validator and user auth to data analysis endpoint

--- server/src/controllers/create-data-analysis.js
+++ server/src/controllers/create-data-analysis.js
@@ -1,5 +1,49 @@
+const { StatusCode } = require('status-code-enum');
+
 const Location = require('../models/location');
 const Recommender = require('../core/recommender');
+const Authorization = require('../core/authorization');
+const User = require('../models/user');
+const wrapper = require('./assets/wrapper');
+
+/**
+ * Validator function the data analysis endpoint.
+ *
+ * @author Niclas Kühnapfel
+ * @param request {e.Request}
+ * @param response {e.Response}
+ * @param next
+ */
+function validator(request, response, next) {
+  let valid = true;
+  const userId = request.body.user_id;
+
+  if (userId) {
+    if (!User.validateID(userId)) {
+      valid = false;
+    }
+  }
+
+  const { location } = request.body;
+
+  if (location) {
+    valid = valid
+      && location
+      && Location.isLatitude(location.latitude)
+      && Location.isLongitude(location.longitude);
+  } else {
+    valid = false;
+  }
+
+  if (!valid) {
+    response.status(StatusCode.ClientErrorBadRequest).send();
+    return;
+  }
+
+  next();
+}
+
+exports.validator = wrapper(validator);
 
 /**
  * Controller function for the data analysis endpoint.
@@ -8,21 +52,28 @@ const Recommender = require('../core/recommender');
  * @param {e.Request} request - An express request object.
  * @param {e.Response} response - An express response object.
  */
-async function createDataAnalysis(request, response) {
-  let location;
-  try {
-    location = new Location(
-      request.body.location.latitude,
-      request.body.location.longitude
-    );
-  } catch (e) {
-    response.json(e.toString());
-    return;
+async function controller(request, response) {
+  const location = new Location(
+    request.body.location.latitude,
+    request.body.location.longitude
+  );
+
+  const userId = request.body.user_id;
+  const { authorization } = request.headers;
+
+  if (userId) {
+    const accessToken = Authorization.extractAccessToken(authorization);
+    const user = new User(userId);
+
+    if (!(await Authorization.authorizeUser(user, accessToken))) {
+      response.status(StatusCode.ClientErrorUnauthorized).send();
+      return;
+    }
   }
 
   const recommender = new Recommender();
-  const recommendations = await recommender.getRecommendations(location);
-  response.json({ violation: recommendations });
+  const data = await recommender.getRecommendations(location, userId);
+  response.json({ violation: data });
 }
 
-module.exports = createDataAnalysis;
+exports.controller = wrapper(controller);
